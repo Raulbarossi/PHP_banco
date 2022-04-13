27f9d254912c1288029fd9acd51cfc6b3e4a6be3
Barossi <raul_barossi@hotmail.com>
2022-04-13 04:53:07 -0300

    Correção dps da refatoração

--- banco.php
+++ banco.php
@@ -12,3 +12,16 @@ $nivanConta->transfere(500, $lucasConta);
 $nivanConta->exibeSaldo();
 $lucasConta->exibeSaldo();
 conta::contagemContas();
+
+/*          RESULTADOS / LX = LINHA X
+L4  - Lucas Silva, sua conta foi criada com sucesso, seu cpf é 236.589.654-55.
+L5  - Deposito de R$ 1.555,47 realizado com sucesso, Seu saldo atual é R$ 1.555,47
+L6  - Saque de R$ 300,50 realizado com sucesso retire as cédulas no local indicado, Seu saldo atual é R$ 1.254,97
+L7  - Sua tentativa de saque foi R$ -300,50, mas o valor do saque deve ser positivo.
+L8  - Lucas Silva Seu saldo é de R$ 1.254,97
+L9  - Nivaldo Junior, sua conta foi criada com sucesso, seu cpf é 356.452.315-30.
+L10 - Deposito de R$ 1.500,00 realizado com sucesso, Seu saldo atual é R$ 1.500,00
+L11 - Transferência de R$ 500,00 realizado com sucesso, Seu saldo atual é R$ 1.000,00
+L12 - Nivaldo Junior Seu saldo é de R$ 1.000,00
+L13 - Lucas Silva Seu saldo é de R$ 1.754,97
+L14 - O Numero atual é de 2 contas    */
